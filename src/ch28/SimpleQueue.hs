7bd979c0c74962a54eb213abe0da1cbc8b2e9b7b
Tao Yang <our1944@gmail.com>
2016-12-12 22:27:10 +0100

    benchmark simple queue

--- src/ch28/SimpleQueue.hs
+++ src/ch28/SimpleQueue.hs
@@ -1,5 +1,7 @@
 module SimpleQueue where
 
+import Criterion.Main
+
 data Queue a =
   Queue { enqueue :: [a]
         , dequeue :: [a]
@@ -22,3 +24,25 @@ npush x (NQ xs) = NQ $ x : xs
 npop :: NaiveQ a -> Maybe (a, NaiveQ a)
 npop (NQ []) = Nothing
 npop (NQ xs) = let (xs', [x]) = splitAt (length xs - 1)  xs in Just (x, NQ xs')
+
+
+benchQ :: Int -> Queue Int
+benchQ i = let go n q
+                 | n == 0    = q
+                 | odd n     = go (n - 1) (push n q)
+                 | otherwise = go (n - 1) (maybe (Queue [] []) snd (pop q))
+           in go i (Queue [1..i] [])
+
+
+benchNQ :: Int -> NaiveQ Int
+benchNQ i = let go n q
+                  | n == 0    = q
+                  | odd n     = npush n q
+                  | otherwise = go (n - 1) (maybe (NQ []) snd (npop q))
+            in go i (NQ [1..i])
+
+main :: IO ()
+main = defaultMain
+  [ bench "simple queue" $ whnf benchQ 123456
+  , bench "naive queue" $ whnf benchNQ 123456
+  ]
