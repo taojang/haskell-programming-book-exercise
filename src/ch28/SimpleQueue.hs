5a720e0253ef2af64dbd80434720bf654b7ddf1a
Tao Yang <our1944@gmail.com>
2016-12-14 01:32:30 +0100

    fixes simple queue

--- src/ch28/SimpleQueue.hs
+++ src/ch28/SimpleQueue.hs
@@ -23,23 +23,22 @@ npush x (NQ xs) = NQ $ x : xs
 
 npop :: NaiveQ a -> Maybe (a, NaiveQ a)
 npop (NQ []) = Nothing
---npop (NQ xs) = Just (last xs, NQ (init xs))
-npop (NQ xs) = let (xs', [x]) = splitAt (length xs - 1)  xs in Just (x, NQ xs')
+npop (NQ xs) = Just (last xs, NQ (init xs))
 
 
 benchQ :: Int -> Queue Int
 benchQ i = let go n q
-                 | n == 0    = q
-                 | even n    = go (n - 1) (push n q)
-                 | otherwise = go (n - 1) (maybe (Queue [] []) snd (pop q))
+                 | n == 0       = q
+                 | mod n 2 == 0 = go (n - 1) (maybe (Queue [] []) snd (pop q))
+                 | otherwise    = go (n - 1) (push n q)
            in go i (Queue [] [])
 
 
 benchNQ :: Int -> NaiveQ Int
 benchNQ i = let go n q
-                  | n  == 0   = q
-                  | even n    = go (n - 1) (npush n q)
-                  | otherwise = go (n - 1) (maybe (NQ []) snd (npop q))
+                  | n  == 0      = q
+                  | mod n 2 == 0 = go (n - 1) (maybe (NQ []) snd (npop q))
+                  | otherwise    = go (n - 1) (npush n q)
             in go i (NQ [])
 
 main :: IO ()
