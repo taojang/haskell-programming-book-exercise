187fa369f167c2674f4c01e3fd78f2110eb8175d
Tao Yang <our1944@gmail.com>
2016-12-14 01:25:12 +0100

    fixes simplequeue

--- src/ch28/SimpleQueue.hs
+++ src/ch28/SimpleQueue.hs
@@ -12,34 +12,35 @@ push x (Queue enq deq) = Queue (x : enq) deq
 
 pop :: Queue a -> Maybe (a, Queue a)
 pop (Queue []  [] ) = Nothing
-pop (Queue [x] [] ) = Just (x, Queue [] [])
 pop (Queue enq [] ) = let x : deq  = reverse enq in Just (x, Queue [] deq)
-pop (Queue enq deq) = let x : deq' = deq in Just (x, Queue enq deq')
+pop (Queue enq (x:xs)) = Just (x, Queue enq xs)
 
 newtype NaiveQ a = NQ { unNQ :: [a] }
+  deriving (Show, Eq)
 
 npush :: a -> NaiveQ a -> NaiveQ a
 npush x (NQ xs) = NQ $ x : xs
 
 npop :: NaiveQ a -> Maybe (a, NaiveQ a)
 npop (NQ []) = Nothing
+--npop (NQ xs) = Just (last xs, NQ (init xs))
 npop (NQ xs) = let (xs', [x]) = splitAt (length xs - 1)  xs in Just (x, NQ xs')
 
 
 benchQ :: Int -> Queue Int
 benchQ i = let go n q
                  | n == 0    = q
-                 | odd n     = go (n - 1) (push n q)
+                 | even n    = go (n - 1) (push n q)
                  | otherwise = go (n - 1) (maybe (Queue [] []) snd (pop q))
-           in go i (Queue [1..i] [])
+           in go i (Queue [] [])
 
 
 benchNQ :: Int -> NaiveQ Int
 benchNQ i = let go n q
-                  | n == 0    = q
-                  | odd n     = go (n - 1) (npush n q)
+                  | n  == 0   = q
+                  | even n    = go (n - 1) (npush n q)
                   | otherwise = go (n - 1) (maybe (NQ []) snd (npop q))
-            in go i (NQ [1..i])
+            in go i (NQ [])
 
 main :: IO ()
 main = defaultMain
